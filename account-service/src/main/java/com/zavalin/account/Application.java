9f6c216608aa5fc9c987daced4b63fa61fbc2e4a
Andrey Zavalin <garedok@gmail.com>
2017-06-07 02:48:29 +0300

    changes

--- account-service/src/main/java/com/zavalin/account/Application.java
+++ account-service/src/main/java/com/zavalin/account/Application.java
@@ -62,15 +62,14 @@ public class Application extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll();
-//                .antMatchers("/registration").permitAll()
-//                    .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                    .loginPage("/login")
-//                    .permitAll()
-//                .and()
-//                .logout()
-//                    .permitAll();
+                .antMatchers("/registration").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                .logout()
+                    .permitAll();
     }
 }
